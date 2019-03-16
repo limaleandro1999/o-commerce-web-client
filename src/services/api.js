8f6f1a0f4e31573524b912983e43f9adc4b79953
Leandro Lima <limaleandro1999@gmail.com>
2019-03-16 01:05:01 +0000

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://o-commerce-web-server.herokuapp.com/'//'http://localhost:3000'
 });
 
 export default api;
